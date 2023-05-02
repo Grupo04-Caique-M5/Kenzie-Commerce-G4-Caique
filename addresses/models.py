8867dbd469f3035895ed56f87958520fdff28e4e
Jorge Rodrigo <86239864+Jorge-Rodrigo@users.noreply.github.com>
2023-05-02 16:44:17 -0300

    fix: model name

--- addresses/models.py
+++ addresses/models.py
@@ -1,12 +1,12 @@
 from django.db import models
 
 
-class Adress(models.Model):
+class Address(models.Model):
     number = models.IntegerField()
     street = models.CharField(max_length=255)
     city = models.CharField(max_length=55)
     state = models.CharField(max_length=2)
 
     user = models.OneToOneField(
-        "users.User", on_delete=models.PROTECT, related_name="adress"
+        "users.User", on_delete=models.PROTECT, related_name="address"
     )
