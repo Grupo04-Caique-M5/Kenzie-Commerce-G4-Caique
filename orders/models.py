b0f38d7af9ab5072cb81be2337dd85373a510dfc
gusdinizmaia <gustavodiniz37@hotmail.com>
2023-05-06 18:06:05 -0300

    feat: post orders/ created, missing create for differents vendors

--- orders/models.py
+++ orders/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.contrib.postgres.fields import JSONField
 
 
 class StatusChoices(models.TextChoices):
@@ -9,14 +10,14 @@ class StatusChoices(models.TextChoices):
 
 class Order(models.Model):
     status = models.CharField(
-        max_length=30, choices=StatusChoices.choices, default=StatusChoices.ORDER_RECEIVED
+        max_length=30,
+        choices=StatusChoices.choices,
+        default=StatusChoices.ORDER_RECEIVED,
     )
     order_time = models.TimeField(auto_now_add=True)
     total_price = models.DecimalField(max_digits=8, decimal_places=2)
     vendor_id = models.IntegerField(default=0)
+    cart_products = models.JSONField(default={})
     user = models.ForeignKey(
         "users.User", on_delete=models.PROTECT, related_name="user_orders"
-    )
-    cart = models.ForeignKey(
-        "carts.Cart", on_delete=models.PROTECT, related_name="cart_orders"
     )
--- orders/serializers.py
+++ orders/serializers.py
@@ -1,18 +1,31 @@
 from rest_framework import serializers
 from .models import Order
+from products.serializers import ProductSerializer
+from products.models import Product
+from carts.serializers import CartProductSerializer
 
 
 class OrderSerializer(serializers.ModelSerializer):
+    cart_products = CartProductSerializer(
+        read_only=True, many=True, source="user.cart.cart_cart"
+    )
+
     def create(self, validated_data):
         user = validated_data.pop("user")
         cart = user.cart
 
+        cart_products = user.cart.cart_cart.filter(cart=cart).values_list(
+            "product", flat=True
+        )
+        new_products = [Product.objects.get(id=product) for product in cart_products]
+        new_products_dict = ProductSerializer(new_products, many=True)
+
+        validated_data["cart_products"] = new_products_dict.data
+
         prices_list = user.cart.cart_cart.filter(cart=cart).values_list(
             "product__price", flat=True
         )
-
         prices_list = [price for price in prices_list]
-
         prices = list(prices_list)
 
         validated_data["total_price"] = sum(prices)
@@ -20,15 +33,16 @@ class OrderSerializer(serializers.ModelSerializer):
         vendors_list = user.cart.cart_cart.filter(cart=cart).values_list(
             "product__user", flat=True
         )
-
         vendors = list(set(vendors_list))
 
         for vendor in vendors:
             validated_data["vendor_id"] = vendor
             print(validated_data)
-            # order = Order.objects.create(**validated_data, user=user, cart=cart)
+            return Order.objects.create(**validated_data, user=user)
+        
+        #falta criar para vendores diferentes
 
     class Meta:
         model = Order
-        fields = ["total_price", "vendor_id", "user", "cart"]
-        read_only_fields = ["user", "cart"]
+        fields = ["total_price", "vendor_id", "user", "cart_products"]
+        read_only_fields = ["user", "total_price"]
