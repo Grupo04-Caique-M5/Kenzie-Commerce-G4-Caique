98635b3ab699440e0d56eabb2882bf4aa6072065
Luan Florencio <luanflorencio.dev@gmail.com>
2023-05-09 10:39:34 -0300

    feat: add docs

--- _kenzie_comerce/urls.py
+++ _kenzie_comerce/urls.py
@@ -16,6 +16,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import include, path
+from drf_spectacular.views import (
+    SpectacularAPIView,
+    SpectacularRedocView,
+)
 
 
 urlpatterns = [
@@ -24,4 +28,6 @@ urlpatterns = [
     path("api/", include("products.urls")),
     path("api/", include("carts.urls")),
     path("api/", include("orders.urls")),
+    path("api/schema/", SpectacularAPIView.as_view(), name="schema"),
+    path("api/docs/", SpectacularRedocView.as_view(url_name="schema"), name="redoc"),
 ]
